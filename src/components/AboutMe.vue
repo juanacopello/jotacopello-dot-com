64935d5c4976e957f9d162db6df5ad3183ffb6c5
Juana Copello <juanacopello@github.com>
2023-02-06 01:27:10 -0300

    cambios presentacion

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,13 +3,11 @@
     <section id="myBio">
       <h3>¡Hola!</h3>
       <p>
-        My name is Juana Copello. I am a journalist specializing in Data Visualization and Visual Storytelling. 
+        My name is Juana Copello. I am a journalist specializing in <strong>Data Visualization and Visual Storytelling</strong>. 
         <br />
-        I am currently working at the Graphics Desk at LA NACION, a a national news media outlet in Argentina, where I research for content and develop interactive visual stories.
+        I am currently working at the Graphics Desk at LA NACION, a a national news media outlet in Argentina, where I research and develop interactive visual stories.
         I am also an Assistant Professor at the Universidad Torcuato Di Tella (UTDT) Design School, teaching introductory coding and data visualization classes to undergraduate students.
-        <br />
-        This site is on english pero mi idioma nativo es el español.
-      </p>
+        </p>
     </section>
   </div>
 </template>
