6e38f0bb8f8234d2d1bbfde8a43feff9ca5ac3d8
Juana Copello <copellojuana@gmail.com>
2025-01-15 20:26:31 -0300

    cambios premios y notas

--- src/components/Awards.vue
+++ src/components/Awards.vue
@@ -5,7 +5,7 @@
       <div class="list-container">
         <ul>
           <li v-for="(award, i) in listAwards" :key="i">
-            {{ award.year }} | <span>{{ award.type }}</span> | {{ award.organization }} |
+            <span class="year__award">{{ award.year }}</span> | <span>{{ award.type }}</span> | {{ award.organization }} |
             <a class="awarded-project-url" :href="award.publicationUrl" target="_blank"> {{ award.publicationText }} </a>
           </li>
         </ul>
@@ -21,28 +21,24 @@
 }
 
 .list-container ul li{
-  margin: 20px auto;
+  margin: 10px auto;
+  font-size: 16px;
+}
+
+.year__award{
+  font-weight: 700;
 }
 
 
 .list-container ul li a{
   color: var(--color-text);
-  /* text-decoration: none; */
+  text-decoration: none;
 }
 
 .awarded-project-url{
-  box-shadow: inset 0 0 0 0 #54b3d6;
-  color: #54b3d6;
-  margin: 0 -.25rem;
-  padding: 0 .25rem;
-  transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
+  font-style: italic;
 }
 
-.awarded-project-url:hover {
-  box-shadow: inset 100% 0 0 0 #54b3d6;
-  color: red;
-  color: rgba(0, 0, 0, 0.671);
-}
 </style>
 
 <script>
@@ -52,60 +48,84 @@ export default {
     return {
       listAwards: [
         {
-          year: 2023,
-          type: "Silver",
+          year: 2024,
+          type: "Silver Medal",
           organization: "Society for News Design",
           publicationText: "Presidential Debates in Argentina",
           publicationUrl: "https://www.lanacion.com.ar/politica/quien-gano-el-debate-presidencial-2023-los-aciertos-y-errores-de-los-candidatos-segun-los-columnitas-nid02102023/#/",
         },
         {
+          year: 2024,
+          type: "First Place",
+          organization: "Asociación de Entidades Periodísticas Argentinas",
+          publicationText: "Dengue, a Journey into the Epidemic: This is How it Acts and Spreads",
+          publicationUrl: "https://www.lanacion.com.ar/sociedad/dengue-viaje-al-interior-de-la-epidemia-asi-actua-el-virus-dentro-de-tu-cuerpo-y-asi-se-propaga-nid11042024/#//",
+        },
+        {
           year: 2023,
-          type: "First place",
-          organization: "Grupo de Diarios América",
-          publicationText: "Rosario, a Drug Feud",
-          publicationUrl: "https://www.lanacion.com.ar/politica/rosario-feudo-narco-el-imperio-criminal-que-se-administra-detras-de-las-rejas-con-complicidad-nid30102022/#/",
+          type: "Award of Excellence",
+          organization: "Society for News Design",
+          publicationText: "Step by Step, the Artemis Mission that Will Return Man to the Moon",
+          publicationUrl: "https://www.lanacion.com.ar/sociedad/primero-la-luna-luego-marte-medio-siglo-despues-del-apolo-11-estados-unidos-quiere-volver-a-nid28082022/#/",
+        },
+        {
+          year: 2023,
+          type: "Second Place",
+          organization: "Asociación de Entidades Periodísticas Argentinas",
+          publicationText: "Step by Step, the Artemis Mission that Will Return Man to the Moon",
+          publicationUrl: "https://www.lanacion.com.ar/sociedad/primero-la-luna-luego-marte-medio-siglo-despues-del-apolo-11-estados-unidos-quiere-volver-a-nid28082022/#/",
+        },
+   
+        
+        {
+          year: 2022,
+          type: "Bronze Medal",
+          organization: "Society for News Design",
+          publicationText: "2021 Midterm Elections in Argentina",
+          publicationUrl: "https://www.lanacion.com.ar/politica/elecciones-2021-mapa-de-resultados-en-tiempo-real-nid15112021/",
         },
         {
           year: 2022,
           type: "Award of Excellence",
           organization: "Society for News Design",
-          publicationText: "Incriminating Messages. This is How the Attack against Cristina Kirchner was Planned and Carried Out",
-          publicationUrl: "https://www.lanacion.com.ar/politica/mensajes-delatores-asi-se-planifico-y-ejecuto-el-atentado-contra-cristina-kirchner-nid17092022/#/",
-        },
-        {
-          year: 2021,
-          type: "First Place",
-          organization: "Sociedad Interamericana de Prensa",
-          publicationText: "Step by Step, The Artemis Mission Plan to Return the Man to the Moon",
-          publicationUrl: "https://www.lanacion.com.ar/sociedad/primero-la-luna-luego-marte-medio-siglo-despues-del-apolo-11-estados-unidos-quiere-volver-a-nid28082022/#/",
-        },
-        {
-          year: 2021,
-          type: "Award of Excellence",
-          organization: "Society for News Design",
-          publicationText: "Plaza de Mayo on Fire. How the Five Deaths that Changed Argentina's History Took Place",
+          publicationText: "Plaza de Mayo on Flames. How the Five Deaths that Changed Argentina's History Took Place",
           publicationUrl: "https://www.lanacion.com.ar/politica/plaza-de-mayo-en-llamas-asi-ocurrieron-las-cinco-muertes-que-cambiaron-la-historia-de-la-argentina-nid19122021/",
         },
         {
-          year: 2021,
+          year: 2022,
           type: "Award of Excellence",
           organization: "Society for News Design",
           publicationText: "Simulation. This is How the Virus can Spread in Public Transport",
           publicationUrl: "https://www.lanacion.com.ar/sociedad/simulacion-asi-se-puede-propagar-el-virus-en-el-transporte-nid11042021/",
         },
         {
+          year: 2022,
+          type: "Recognition",
+          organization: "Sociedad Interamericana de Prensa",
+          publicationText: "Plaza de Mayo on Flames. How the Five Deaths that Changed Argentina's History Took Place",
+          publicationUrl: "https://www.lanacion.com.ar/politica/plaza-de-mayo-en-llamas-asi-ocurrieron-las-cinco-muertes-que-cambiaron-la-historia-de-la-argentina-nid19122021/",
+        },
+      
+        {
           year: 2021,
-          type: "Award of Excellence",
-          organization: "Society for News Design",
-          publicationText: "How the Covid-19 Vaccines Work",
+          type: "First Place",
+          organization: "Sociedad Interamericana de Prensa",
+          publicationText: "How Covid-19 Vaccines Work",
           publicationUrl: "https://www.lanacion.com.ar/sociedad/asi-actuan-vacunas-carrera-coronavirus-nid2406017/",
         },
         {
-          year: 2020,
+          year: 2021,
           type: "Award of Excellence",
           organization: "Society for News Design",
-          publicationText: "15th Anniversary of Cromañon's Tragedy",
-          publicationUrl: "https://especialess3.lanacion.com.ar/19/12/3d-cromanon/",
+          publicationText: "How Covid-19 Vaccines Work",
+          publicationUrl: "https://www.lanacion.com.ar/sociedad/asi-actuan-vacunas-carrera-coronavirus-nid2406017/",
+        },
+        {
+          year: 2021,
+          type: "Award of Excellence",
+          organization: "Society for News Design",
+          publicationText: "SARS-CoV-2: Day by Day, How the Virus Affects the Body",
+          publicationUrl: "https://www.lanacion.com.ar/sociedad/dia-dia-como-actua-virus-organismo-nid2358830/",
         },
       ],
     };
--- src/components/ProjectsLN.vue
+++ src/components/ProjectsLN.vue
@@ -202,24 +202,15 @@ import discursosGif from "@/images/discursos.gif";
 import gifTemperatura from "@/images/mapa_temperatura.gif";
 import mapaElectoral from "@/images/mapa-electoral-generales.gif";
 import proyectoCalle from "@/images/proyecto-calle-1.png";
-import argendata from "@/images/argendata.png"
-
+import argendata from "@/images/argendata.png";
+import gifCompass from "@/images/compass.gif";
+import gifCharts from "@/images/charts.gif";
 
 export default {
   name: "ProjectsLN",
   data() {
     return {
       articles: [
-         //Simulación en el colectivo
-         {
-          title:
-            "Argendata: Data to Understand Argentina",
-          image: argendata,
-          alt: "argendata_captura",
-          link: "https://argendata.fund.ar/",
-          published: "Fundar",
-          role: ["Data visualization"],
-        },
         //Simulación en el colectivo
         {
           title:
@@ -231,17 +222,17 @@ export default {
           published: "La Nacion",
           role: ["Reporting"],
         },
-        //Aplausómetro
+        //Islas de calor
         {
           title:
-            "Justice, Mauricio Macri, Debt and Abortion: Year by Year, Which Theme was the Most Clapped in Each of the President's Speech in Congress.",
-          image:
-            "https://resizer.glanacion.com/resizer/bgzZwVQheMcpBo7UsqTz3HjbJMU=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/PYNUBRJEH5HBTCJEBF3RWG4HXA.jpg",
-          alt: "alberto_fernandez_speech",
-          link: "https://www.lanacion.com.ar/politica/macri-deuda-y-aborto-aplausometro-ano-a-ano-cuanto-y-por-que-aplaudieron-a-alberto-fernandez-en-el-nid01032023/",
+            "Heat Islands: These Are the Corners in Buenos Aires Where the Temperature is Hell",
+          image: gifTemperatura,
+          alt: "heat-islands-gif",
+          link: "https://www.lanacion.com.ar/sociedad/islas-de-calor-portenas-tenes-calor-entonces-ni-te-asomes-a-estas-esquinas-portenas-donde-la-nid06022024/#/",
           published: "La Nacion",
-          role: ["Data analysis", "Reporting"],
+          role: ["Web development"],
         },
+
         //Distorsión de precios
         {
           title:
@@ -268,42 +259,20 @@ export default {
           image:
             "https://resizer.glanacion.com/resizer/1efleqADafaG5DwG7uvgaRMDt6U=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/WXIM3KE6QFDTNPNMTLEBTK2BTE.gif",
           alt: "plataformas_vacunas_covid19_gif",
-          link: "https://www.lanacion.com.ar/sociedad/asi-actuan-vacunas-carrera-coronavirus-nid2406017/",
+          link: "https://www.lanacion.com.ar/sociedad/vacunas-covid-19-cuales-llegaran-argentina-que-resultados-nid2526910/",
           published: "La Nacion",
           role: ["Reporting"],
         },
-        //Diferencia General
+        //Aplausómetro
         {
           title:
-            "Can Javier Milei Win in the First Round? The Feat that he Seeks and that Challenges Electoral Statistics",
-          image: difGeneralPaso,
-          alt: "dif_general_paso_captura",
-          link: "https://www.lanacion.com.ar/politica/puede-ganar-en-primera-vuelta-la-hazana-que-busca-javier-milei-y-que-desafia-a-las-estadisticas-nid21102023/",
-          published: "La Nacion",
-          role: ["Data visualization"],
-        },
-  
-
-        //Elecciones 2023: Mapa de resultados en todo el país
-        {
-          title:
-            "The New Political Map: the Results of the 2023 General Elections",
-          image: mapaElectoral,
-          alt: "mapa_resultados_en_el_pais_2023",
-          link: "https://www.lanacion.com.ar/politica/elecciones-2023-los-resultados-de-los-comicios-presidenciales-en-todo-el-pais-nid22102023/#/presidente",
-          published: "La Nacion",
-          role: ["data reporting"],
-        },
-        //Mapa de intendentes
-        {
-          title:
-            "Mayor Map: Winners and Losers in the Districts of the Buenos Aires Province",
+            "Justice, Mauricio Macri, Debt and Abortion: Year by Year, Which Theme was the Most Clapped in Each of the President's Speech in Congress.",
           image:
-            "https://resizer.glanacion.com/resizer/v2/-G7FFCPNQCFBAFN6VMGJZ72O4EY.jpg?auth=f941ef7bd17d0fc74925b07468c7a0bf40ceb391d72339f20fa186a36f3b98d3&width=768&quality=70&smart=false",
-          alt: "mayor-map-image",
-          link: "https://www.lanacion.com.ar/politica/elecciones-2023-el-nuevo-mapa-de-intendentes-bonaerenses-nid22102023/#/intendentes",
+            "https://resizer.glanacion.com/resizer/bgzZwVQheMcpBo7UsqTz3HjbJMU=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/PYNUBRJEH5HBTCJEBF3RWG4HXA.jpg",
+          alt: "alberto_fernandez_speech",
+          link: "https://www.lanacion.com.ar/politica/macri-deuda-y-aborto-aplausometro-ano-a-ano-cuanto-y-por-que-aplaudieron-a-alberto-fernandez-en-el-nid01032023/",
           published: "La Nacion",
-          role: ["Reporting"],
+          role: ["Data analysis", "Reporting"],
         },
         //Viajes de precandidatos en todo el país
         {
@@ -315,7 +284,29 @@ export default {
           published: "La Nacion",
           role: ["Web development"],
         },
-        
+        //Dengue
+        {
+          title:
+            "Dengue. This is How the Virus Acts Inside your Body and How it Spreads",
+          image:
+            "https://resizer.glanacion.com/resizer/ZBq-a_kAPECZPeFZmXtKvRg02vA=/375x250/smart/filters:format(webp):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/IWZGCFVLKNFNJLJDFF7RZA3WDE.jpg",
+          alt: "mosquito",
+          link: "https://www.lanacion.com.ar/sociedad/dengue-viaje-al-interior-de-la-epidemia-asi-actua-el-virus-dentro-de-tu-cuerpo-y-asi-se-propaga-nid11042024/#/",
+          published: "La Nacion",
+          role: ["Reporting"],
+        },
+
+        //Diferencia General
+        {
+          title:
+            "Can Javier Milei Win in the First Round? The Feat that he Seeks and that Challenges Electoral Statistics",
+          image: difGeneralPaso,
+          alt: "dif_general_paso_captura",
+          link: "https://www.lanacion.com.ar/politica/puede-ganar-en-primera-vuelta-la-hazana-que-busca-javier-milei-y-que-desafia-a-las-estadisticas-nid21102023/",
+          published: "La Nacion",
+          role: ["Data visualization"],
+        },
+
         //Artemis
         {
           title:
@@ -339,16 +330,28 @@ export default {
           role: ["Reporting"],
         },
 
-              //Proyecto Calle
-              {
+        //Elecciones 2023: Mapa de resultados en todo el país
+        {
           title:
-            "Living in the Streets: What Do people who Don't Have a Bed to Spend the Night Dream About?",
-          image: proyectoCalle,
-          alt: "proyecto_calle",
-          link: "https://www.lanacion.com.ar/comunidad/con-que-suenan-las-personas-que-no-tienen-ni-una-cama-donde-pasar-la-noche-nid29062024/#/",
+            "The New Political Map: the Results of the 2023 General Elections",
+          image: mapaElectoral,
+          alt: "mapa_resultados_en_el_pais_2023",
+          link: "https://www.lanacion.com.ar/politica/elecciones-2023-los-resultados-de-los-comicios-presidenciales-en-todo-el-pais-nid22102023/#/presidente",
           published: "La Nacion",
-          role: ["web development"],
+          role: ["data reporting"],
         },
+        //Mapa de intendentes
+        {
+          title:
+            "Mayor Map: Winners and Losers in the Districts of the Buenos Aires Province",
+          image:
+            "https://resizer.glanacion.com/resizer/v2/-G7FFCPNQCFBAFN6VMGJZ72O4EY.jpg?auth=f941ef7bd17d0fc74925b07468c7a0bf40ceb391d72339f20fa186a36f3b98d3&width=768&quality=70&smart=false",
+          alt: "mayor-map-image",
+          link: "https://www.lanacion.com.ar/politica/elecciones-2023-el-nuevo-mapa-de-intendentes-bonaerenses-nid22102023/#/intendentes",
+          published: "La Nacion",
+          role: ["Reporting"],
+        },
+
         //Cards debate
         {
           title:
@@ -369,39 +372,7 @@ export default {
           published: "La Nacion",
           role: ["Web development"],
         },
-        //Dengue
-        {
-          title:
-            "Dengue. This is How the Virus Acts Inside your Body and How it Spreads",
-          image:
-            "https://resizer.glanacion.com/resizer/ZBq-a_kAPECZPeFZmXtKvRg02vA=/375x250/smart/filters:format(webp):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/IWZGCFVLKNFNJLJDFF7RZA3WDE.jpg",
-          alt: "mosquito",
-          link: "https://www.lanacion.com.ar/sociedad/dengue-viaje-al-interior-de-la-epidemia-asi-actua-el-virus-dentro-de-tu-cuerpo-y-asi-se-propaga-nid11042024/#/",
-          published: "La Nacion",
-          role: ["Reporting"],
-        },
 
-        //Islas de calor
-        {
-          title:
-            "Heat Islands: These Are the Corners in Buenos Aires Where the Temperature is Hell",
-          image: gifTemperatura,
-          alt: "heat-islands-gif",
-          link: "https://www.lanacion.com.ar/sociedad/islas-de-calor-portenas-tenes-calor-entonces-ni-te-asomes-a-estas-esquinas-portenas-donde-la-nid06022024/#/",
-          published: "La Nacion",
-          role: ["Web development"],
-        },
-
-        //Calculadora Electoral
-        {
-          title:
-            "Will There be a Ballottage or Will it be Defined in the First Round? Predict How the Votes will Rearrange in October in the General Election",
-          image: gifCalculadora,
-          alt: "calculator_gif",
-          link: "https://www.lanacion.com.ar/politica/elecciones-2023-la-calculadora-para-proyectar-como-se-reacomodaran-los-votos-en-la-primera-vuelta-nid26082023/",
-          published: "La Nacion",
-          role: ["Reporting"],
-        },
         //Cómo ataca el virus en el organismo
         {
           title: "SARS-CoV-2: Day by Day, How the Virus Affect the Body",
@@ -414,12 +385,61 @@ export default {
         },
         //Cromañón
         {
-          title: "15th Anniversary of Cromañón Tragedy",
+          title:
+            "20th Anniversary of Cromañón Tragedy: The Route Through a Sinister Labyrinth",
           image: gifCromanion,
           alt: "cromanion_sinister_maze_gif",
-          link: "https://especialess3.lanacion.com.ar/19/12/3d-cromanon/",
+          link: "https://www.lanacion.com.ar/seguridad/a-20-anos-de-cromanon-recorrido-3d-por-un-laberinto-siniestro-colmado-de-desesperacion-y-muerte-nid29122024/",
           published: "La Nacion",
           role: ["Reporting"],
+        },
+        // Political Compass EEUU 2024
+        {
+          title:
+            "Political Compass:  Test If You are Closer to Harris’ or Trump’s Ideas",
+          image: gifCompass,
+          alt: "compass_gif",
+          link: "https://www.lanacion.com.ar/estados-unidos/political-compass-donald-trump-o-kamala-harris-descubri-cual-es-el-candidato-de-estados-unidos-que-nid27102024/",
+          published: "La Nacion",
+          role: ["Web development"],
+        },
+        /* Inflacion Charts */
+        {
+          title: "Inflation Charts",
+          image: gifCharts,
+          alt: "cromanion_sinister_maze_gif",
+          link: "https://www.lanacion.com.ar/economia/la-inflacion-de-noviembre-fue-de-24-y-acumulo-112-en-el-ano-nid11122024/",
+          published: "La Nacion",
+          role: ["Web development"],
+        },
+        //Calculadora Electoral
+        // {
+        //   title:
+        //     "Will There be a Ballottage or Will it be Defined in the First Round? Predict How the Votes will Rearrange in October in the General Election",
+        //   image: gifCalculadora,
+        //   alt: "calculator_gif",
+        //   link: "https://www.lanacion.com.ar/politica/elecciones-2023-la-calculadora-para-proyectar-como-se-reacomodaran-los-votos-en-la-primera-vuelta-nid26082023/",
+        //   published: "La Nacion",
+        //   role: ["Reporting"],
+        // },
+        //Proyecto Calle
+        {
+          title:
+            "Living in the Streets: What Do people who Don't Have a Bed to Spend the Night Dream About?",
+          image: proyectoCalle,
+          alt: "proyecto_calle",
+          link: "https://www.lanacion.com.ar/comunidad/con-que-suenan-las-personas-que-no-tienen-ni-una-cama-donde-pasar-la-noche-nid29062024/#/",
+          published: "La Nacion",
+          role: ["web development"],
+        },
+        //Argendata
+        {
+          title: "Argendata: Data to Understand Argentina",
+          image: argendata,
+          alt: "argendata_captura",
+          link: "https://argendata.fund.ar/",
+          published: "Fundar",
+          role: ["Data visualization"],
         },
       ],
     };
